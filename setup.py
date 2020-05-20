27623e8ce2d74e72580d732256274f537fd4d663
Seaton Ullberg <seatonullberg@gmail.com>
2020-05-20 11:33:18 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="atompack",
+    version="0.1.0",
+    author="Seaton Ullberg",
+    author_email="seatonullberg@gmail.com",
+    description="A flexible Python library for atomic structure generation",
+    long_description=long_description,
+    long_description_content="text/markdown"
+    url="https://github.com/seatonullberg/atompack-py",
+    packages=setuptools.find_packages()
+    license="MIT",
+    install_requires=["numpy"]
+)
