75162b6273e267fd26e5aba9ba6b6f42b8b91751
Seaton Ullberg <seatonullberg@gmail.com>
2020-05-22 01:11:58 -0400

    Add temporary debug plots to crystal.py

--- atompack/crystal.py
+++ atompack/crystal.py
@@ -1,8 +1,11 @@
 from atompack.atom import Atom, AtomCollection
-from atompack.internal import metric_tensor, is_point_in_polyhedron
+from atompack.internal import metric_tensor, is_point_in_polyhedron, rotation_matrix_from_vectors
 
 import numpy as np
 from typing import List, Optional, Tuple
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 class Crystal(AtomCollection):
@@ -75,25 +78,24 @@ class Crystal(AtomCollection):
 
         # process the size argument
         if self._size is None:
-            self._size = (0, 0, 0)
+            self._size = (1, 1, 1)
 
         # use QR decomposition to find the appropriate basis if another orientation is provided
         oriented_vectors = np.zeros((3, 3))
         if self._orientation is None:
             oriented_vectors = lattice_vectors
         else:
-            r = np.abs(np.linalg.qr(self._orientation, mode="r"))
+            q, r = np.abs(np.linalg.qr(self._orientation))
             for i in range(3):
                 for j in range(3):
                     r[i][j] *= np.linalg.norm(lattice_vectors[i])
             oriented_vectors = r
 
         # generate a rotation matrix to align the natural basis with the new orientation
-        # https://stackoverflow.com/questions/50474886/finding-rotation-matrix-to-transform-one-3-vector-basis-to-another-in-3d
-        rotation_matrix = np.linalg.solve(lattice_vectors, oriented_vectors).T
+        rotation_matrices = [rotation_matrix_from_vectors(lattice_vectors[i], q[i]) for i in range(3)]
 
         # scale the basis such that it is (just) larger than necessary to accommodate the new orientation
-        oriented_size = [0, 0, 0]
+        oriented_size = [1, 1, 1]
         for i in range(3):
             oriented_size[i] = np.ceil(
                 np.linalg.norm(oriented_vectors[i]) /
@@ -102,6 +104,9 @@ class Crystal(AtomCollection):
 
         # scale the oriented vectors by the size
         oriented_vectors *= np.array(self._size)
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection="3d")
 
         # place the atoms
         atoms = []
@@ -121,14 +126,37 @@ class Crystal(AtomCollection):
                             position[i] = (np.linalg.norm(lattice_vectors[i]) *
                                            relative_position[i]) + offset[i]
                         print("position: {}".format(position))
-                        position = np.dot(rotation_matrix, position)
+                        ax.scatter(position[0], position[1], position[2], color="blue")
+                        for r_mat in rotation_matrices:
+                            position = np.dot(r_mat, position)
+                        ax.scatter(position[0], position[1], position[2], color="red")
                         print("position rotated: {}\n".format(position))
 
                         # only add the atom to the collection if it falls within the oriented basis
                         if is_point_in_polyhedron(position, oriented_vectors):
                             atom.position = position
                             atoms.append(atom)
+        plt.legend()
+        plt.show("debug.png")
 
         # TODO: apply a rotation to the total collection
 
         return atoms, oriented_vectors
+
+
+if __name__ == "__main__":
+    a, b, c = 1, 1, 1
+    alpha, beta, gamma = np.pi / 2, np.pi / 2, np.pi / 2
+    unit_cell = [(Atom(symbol="Fe"), np.array([0, 0, 0])),
+                 (Atom(symbol="Fe"), np.array([0.5, 0.5, 0.5]))]
+    orientation = np.array([[-1, 1, 0], [0, 0, 1], [1, 1, 0]])
+    #orientation = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    rotation = np.identity(3)
+    size = (1, 1, 1)
+    crystal = Crystal(a, b, c, alpha, beta, gamma, unit_cell, orientation,
+                      rotation, size)
+    assert np.allclose(
+        crystal.basis,
+        np.array([[np.sqrt(2), 0, 0], [0, np.sqrt(2), 0], [0, 0, 1]]))
+    assert len(crystal) == 4
+    assert 1 == 2
