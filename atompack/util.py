c56128dbc5649edd5af053bdfa9e84cb811cd0bf
Seaton Ullberg <seatonullberg@gmail.com>
2020-09-10 01:37:11 -0400

    Fix as_dict implementation

--- atompack/util.py
+++ atompack/util.py
@@ -1,7 +1,15 @@
 from typing import Any, Dict
 
+import numpy as np
+
 
 class AsDict(object):
 
     def as_dict(self) -> Dict[str, Any]:
-        return {k: getattr(self, k) for k in dir(self) if not k.startswith("_")}
+        # collect all public attributes
+        res = {k: getattr(self, k) for k in dir(self) if not k.startswith("_")}
+        # remove callables
+        res = {k: v for k, v in res.items() if not callable(v)}
+        # convert all numpy arrays to list format
+        res = {k: v.tolist() if type(v) is np.ndarray else v for k, v in res.items()}
+        return res
--- tests/crystal_test.py
+++ tests/crystal_test.py
@@ -114,8 +114,7 @@ def test_unit_cell_as_dict():
     assert d["alpha"] == np.pi / 2
     assert d["beta"] == np.pi / 2
     assert d["gamma"] == np.pi / 2
-    assert np.array_equal(d["sites"], np.array([[0.0, 0.0, 0.0], [0.5, 0.5, 0.5]]))
-    assert_array_almost_equal(d["lattice_vectors"], np.array([[2.85, 0.0, 0.0], [0.0, 2.85, 0.0], [0.0, 0.0, 2.85]]))
+    assert d["sites"] == [[0.0, 0.0, 0.0], [0.5, 0.5, 0.5]]
 
 
 def test_crystal_as_dict():
@@ -123,7 +122,6 @@ def test_crystal_as_dict():
     crystal = Crystal(unit_cell)
     d = crystal.as_dict()
     assert d["scale"] == (1, 1, 1)
-    assert np.array_equal(d["orientation"], np.identity(3))
-    assert np.array_equal(d["rotation"], np.identity(3))
-    assert np.array_equal(d["unit_cell"]["sites"], np.array([[0.0, 0.0, 0.0], [0.5, 0.5, 0.5]]))
-    assert_array_almost_equal(d["unit_cell"]["lattice_vectors"], np.array([[2.85, 0.0, 0.0], [0.0, 2.85, 0.0], [0.0, 0.0, 2.85]]))
+    assert d["orientation"] == np.identity(3).tolist()
+    assert d["rotation"] == np.identity(3).tolist()
+    assert d["unit_cell"]["sites"] == [[0.0, 0.0, 0.0], [0.5, 0.5, 0.5]]
