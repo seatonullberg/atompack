b660626bffbaed2ac2c2f328e93fea295fa8f901
Seaton Ullberg <seatonullberg@gmail.com>
2020-10-15 00:06:45 -0400

    Add more benchmarks

--- benches/crystal_test.py
+++ benches/crystal_test.py
@@ -15,7 +15,27 @@ def bench_unit_cell(basis, lattice_parameters, spacegroup):
     return UnitCell(basis, lattice_parameters, spacegroup)
 
 
+def bench_crystal_supercell(basis, lattice_parameters, spacegroup, extent):
+    return Crystal(basis, lattice_parameters, spacegroup).supercell(extent).finish()
+
+
 def test_unit_cell_cubic(benchmark):
     args = get_cubic_unit_cell()
     res = benchmark.pedantic(bench_unit_cell, args, rounds=10, iterations=100)
     assert len(res.atoms) == 4
+
+
+def test_crystal_cubic_supercell_2x2x2(benchmark):
+    args = get_cubic_unit_cell()
+    args = (*args, (2, 2, 2))
+    res = benchmark.pedantic(bench_crystal_supercell, args, rounds=10, iterations=100)
+
+def test_crystal_cubic_supercell_3x3x3(benchmark):
+    args = get_cubic_unit_cell()
+    args = (*args, (3, 3, 3))
+    res = benchmark.pedantic(bench_crystal_supercell, args, rounds=10, iterations=100)
+
+def test_crystal_cubic_supercell_4x4x4(benchmark):
+    args = get_cubic_unit_cell()
+    args = (*args, (4, 4, 4))
+    res = benchmark.pedantic(bench_crystal_supercell, args, rounds=10, iterations=100)
